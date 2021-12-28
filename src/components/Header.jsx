3a7749cff9124e8416a16f39f652bd2fc809eb98
TogrulMustafa <togrulmustafazade8@gmail.com>
2021-12-28 12:26:24 +0400

    Header hissesi bitirildi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Typed from 'react-typed';
+import image from '../img/itBrains.jpeg'
 
 const Header = _ => {
     return (
         <div className='header-wrapper'>
             <div className="main-info">
-                <h1>Web-Development and Services</h1>
+                <h1>Web-Development and Web-Site Services</h1>
                 <Typed
+                className='typed-text'
                 strings={[
                     'Front-end Developer',
                     'Bachelor',
@@ -16,6 +18,7 @@ const Header = _ => {
                     backSpeed={50}
                     loop >
                 </Typed>
+                <a href={image} download className='downloadCV'>Download CV</a>
             </div>
             
         </div>
